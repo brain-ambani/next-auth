e8236448b8c04600b309cac7ebb07e5f03f98dd6
brian-ambani <brainambani1@gmail.com>
2024-04-25 21:51:17 +0300

    add the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Page() {
   return (
-    <div>
-      <h1>Page</h1>
-      <Button>Click me</Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-900">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg ">
+          A simple authentication service using Next.js, Prisma, and JWT.
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
